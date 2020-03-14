5f8d2f546d615702d990a5ec8a0138ce89ead69f
Will Rolls <32841471+TreeFrogBandit@users.noreply.github.com>
2020-03-14 12:28:31 +0000

    Updated
    Added a function to scrape the links to tweets where the paper has been cited (according to altmetric)

--- alt_scrape.py
+++ alt_scrape.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup as bs
 import time
 
 
-def scrape(iop):
+def scrape_handles(iop):
+    print("scraping IOP for twitter handles...")
     n = 0
     results = []
     output = []
     while n < 5:
-        time.sleep(5)  # let's be polite
-        n = n + 1
+        if n != 0:
+            time.sleep(5)  # let's be polite
+        n += 1
         print(f"page {n}")
         r = requests.get(iop + f"twitter/page:{n}")
         results.append(r.text)
@@ -21,9 +23,45 @@ def scrape(iop):
         output = output + x
 
     output = list(set(output))  # deduplicate
+    print("-"*80)
     return output
 
 
+def scrape_links(iop):
+    print("scraping IOP for links to tweets...")
+    n = 0
+    results = []
+    output = []
+    while n < 5:
+        if n != 0:
+            time.sleep(5)  # let's be polite
+        n += 1
+        print(f"page {n}")
+        r = requests.get(iop + f"twitter/page:{n}")
+        results.append(r.text)
+
+    for r in results:
+        soup = bs(r, "lxml")  # import data
+        x = soup.select("time")  # select only <time> tags
+        for i in x:
+            links = i.select("a") #select the links within the <time tags>
+            for link in links:
+                output.append(link.get('href')) #Append the link target to the list
+    output = list(set(output))  # deduplicate
+    print("-"*80)
+    return (output)
+
+
+
+
 if __name__ == "__main__":
-    iop = "https://iop.altmetric.com/details/5152221/"
-    print(scrape(iop))
+    iop = "https://iop.altmetric.com/details/31746378/"
+
+    a = scrape_handles(iop)
+    for i in a:
+        print(i)
+
+
+    a = scrape_links(iop)
+    for i in a:
+        print(i)
